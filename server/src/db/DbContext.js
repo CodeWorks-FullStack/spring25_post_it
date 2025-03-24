221eab06647ff64074a172e88fd78dabd6652aaf
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-24 13:24:38 -0600

    registered schema with mongoose

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
+import { AlbumSchema } from '../models/Album.js';
 
 class DbContext {
-  Account = mongoose.model('Account', AccountSchema);
+  Account = mongoose.model('Account', AccountSchema)
+  Albums = mongoose.model('Album', AlbumSchema)
 }
 
 export const dbContext = new DbContext()
