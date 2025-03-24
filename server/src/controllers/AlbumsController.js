2d93801ad8abc360d7ca7eda6264877625009f4a
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-24 14:01:35 -0600

    get album by id

--- server/src/controllers/AlbumsController.js
+++ server/src/controllers/AlbumsController.js
@@ -7,6 +7,7 @@ export class AlbumsController extends BaseController {
     super('api/albums')
     this.router
       .get('', this.getAllAlbums)
+      .get('/:albumId', this.getAlbumById)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createAlbum)
   }
@@ -31,12 +32,28 @@ export class AlbumsController extends BaseController {
    * @param {import("express").Response} response
    * @param {import("express").NextFunction} next
    */
+  async getAlbumById(request, response, next) {
+    try {
+      const albumId = request.params.albumId
+      const album = await albumsService.getAlbumById(albumId)
+      response.send(album)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+   */
   async createAlbum(request, response, next) {
     try {
       const albumData = request.body
       const userInfo = request.userInfo
       // NOTE whoever makes the request owns the data
       // REVIEW never ever trust the client
+      // NOTE make sure you open the index.d.ts file from types or else you will not get correct intellisense on userInfo
       albumData.creatorId = userInfo.id
       const album = await albumsService.createAlbum(albumData)
       response.send(album)
--- server/src/services/AlbumsService.js
+++ server/src/services/AlbumsService.js
@@ -5,6 +5,10 @@ class AlbumsService {
     const albums = await dbContext.Albums.find().populate('creator', 'name picture')
     return albums
   }
+  async getAlbumById(albumId) {
+    const album = await dbContext.Albums.findById(albumId).populate('creator', 'name picture')
+    return album
+  }
   async createAlbum(albumData) {
     const album = await dbContext.Albums.create(albumData)
     await album.populate('creator', 'name picture')
