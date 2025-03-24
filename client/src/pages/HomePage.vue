1a070f66b22fa5b1b8be3dc66c259bbad9fba30d
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-24 14:48:54 -0600

    updated scss variables

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,17 @@
-<script setup >
-import Example from '@/components/Example.vue';
-
+<script setup>
 
 </script>
 
 <template>
-  <Example />
+  <div class="container text-light">
+    <div class="row">
+      <div class="col-12">
+        <div>
+          <span class="fs-5">Popular Albums</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
