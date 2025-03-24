c7ea3e63294e8ed0eea27df443a3abef18560d96
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-24 15:01:35 -0600

    got albums

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,29 @@
 <script setup>
+import { albumsService } from '@/services/AlbumsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
 
+onMounted(() => {
+  getAlbums()
+})
+
+async function getAlbums() {
+  try {
+    await albumsService.getAlbums()
+  } catch (error) {
+    Pop.error(error, 'Could not get albums')
+    logger.error('COULD NOT GET ALBUMS', error)
+  }
+}
 </script>
 
 <template>
-  <div class="container text-light">
+  <div class="container fredoka-font">
     <div class="row">
       <div class="col-12">
-        <div>
-          <span class="fs-5">Popular Albums</span>
+        <div class="border-bottom border-grey my-2">
+          <span class="fs-5 fw-bold">Popular Albums</span>
         </div>
       </div>
     </div>
---
+++ client/src/services/AlbumsService.js
@@ -0,0 +1,15 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+import { Album } from "@/models/Album.js"
+import { AppState } from "@/AppState.js"
+
+class AlbumsService {
+  async getAlbums() {
+    const response = await api.get('api/albums')
+    logger.log('Got albums', response.data)
+    const albums = response.data.map(pojo => new Album(pojo))
+    AppState.albums = albums
+  }
+}
+
+export const albumsService = new AlbumsService()
