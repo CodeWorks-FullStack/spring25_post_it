2488163871a044a2cbe27665ca25de20989170b2
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-25 10:27:08 -0600

    creating album

--- client/src/components/AlbumForm.vue
+++ client/src/components/AlbumForm.vue
@@ -1,35 +1,62 @@
 <script setup>
+import { albumsService } from '@/services/AlbumsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { ref } from 'vue';
+
 
 const categories = ['aesthetics', 'food', 'games', 'animals', 'misc', 'vibes']
+
+const editableAlbumData = ref({
+  title: '',
+  coverImg: '',
+  description: '',
+  category: ''
+})
+
+async function createAlbum() {
+  try {
+    await albumsService.createAlbum(editableAlbumData.value)
+  } catch (error) {
+    Pop.error(error, 'Could not create album')
+    logger.error('COULD NOT CREATE ALBUM', error)
+  }
+}
 
 </script>
 
 
 <template>
-  <form>
+  <form @submit.prevent="createAlbum()">
     <div class="form-floating mb-3">
-      <input type="text" class="form-control" id="albumTitle" placeholder="Album Title..." minlength="3" maxlength="25"
-        required>
+      <input v-model="editableAlbumData.title" type="text" class="form-control" id="albumTitle"
+        placeholder="Album Title..." minlength="3" maxlength="25" required>
       <label for="albumTitle">Album Title</label>
     </div>
     <div class="form-floating mb-3">
-      <input type="url" class="form-control" id="albumCoverImg" placeholder="Album Cover Image" maxlength="1000"
-        required>
+      <input v-model="editableAlbumData.coverImg" type="url" class="form-control" id="albumCoverImg"
+        placeholder="Album Cover Image" maxlength="1000" required>
       <label for="albumCoverImg">Album Cover Image</label>
     </div>
     <div class="form-floating mb-3">
-      <textarea class="form-control" placeholder="Album Description" id="albumDescription" minlength="15"
-        maxlength="250"></textarea>
+      <textarea v-model="editableAlbumData.description" class="form-control" placeholder="Album Description"
+        id="albumDescription" minlength="15" maxlength="250"></textarea>
       <label for="albumDescription">Album Description</label>
     </div>
     <div class="form-floating mb-3">
-      <select class="form-select" id="albumCategory" aria-label="Album Category" required>
-        <option selected>Choose a category</option>
+      <select v-model="editableAlbumData.category" class="form-select" id="albumCategory" aria-label="Album Category"
+        required>
+        <option value="" selected disabled>Choose a category</option>
         <option v-for="category in categories" :key="'option ' + category" :value="category">
           {{ category }}
         </option>
       </select>
       <label for="albumCategory">Album Category</label>
+    </div>
+    <div class="text-end">
+      <button class="btn btn-success" type="submit">
+        Submit
+      </button>
     </div>
   </form>
 </template>
--- client/src/services/AlbumsService.js
+++ client/src/services/AlbumsService.js
@@ -4,6 +4,10 @@ import { Album } from "@/models/Album.js"
 import { AppState } from "@/AppState.js"
 
 class AlbumsService {
+  async createAlbum(albumData) {
+    const response = await api.post('api/albums', albumData)
+    logger.log('CREATED ALBUM', response.data)
+  }
   async getAlbums() {
     const response = await api.get('api/albums')
     logger.log('Got albums', response.data)
