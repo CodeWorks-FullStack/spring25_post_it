a82756d9c2b0ff96d169c6fd9e66fa72d38285ff
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-25 10:58:32 -0600

    getting album by id

--- client/src/pages/AlbumDetailsPage.vue
+++ client/src/pages/AlbumDetailsPage.vue
@@ -1,10 +1,37 @@
 <script setup>
+import { albumsService } from '@/services/AlbumsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+
+onMounted(() => {
+  getAlbumById()
+})
+
+async function getAlbumById() {
+  try {
+    const albumId = route.params.albumId
+    await albumsService.getAlbumById(albumId)
+  } catch (error) {
+    Pop.error(error, 'Could not get album')
+    logger.error('COULD NOT GET ALBUM')
+  }
+}
 
 </script>
 
 
 <template>
-  Album Details Page!
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        Album page
+      </div>
+    </div>
+  </div>
 </template>
 
 
--- client/src/services/AlbumsService.js
+++ client/src/services/AlbumsService.js
@@ -4,6 +4,7 @@ import { Album } from "@/models/Album.js"
 import { AppState } from "@/AppState.js"
 
 class AlbumsService {
+
   async createAlbum(albumData) {
     const response = await api.post('api/albums', albumData)
     logger.log('CREATED ALBUM', response.data)
@@ -15,6 +16,11 @@ class AlbumsService {
     logger.log('Got albums', response.data)
     const albums = response.data.map(pojo => new Album(pojo))
     AppState.albums = albums
+  }
+
+  async getAlbumById(albumId) {
+    const response = await api.get(`api/albums/${albumId}`)
+    logger.log('GOT ALBUM', response.data)
   }
 }
 
