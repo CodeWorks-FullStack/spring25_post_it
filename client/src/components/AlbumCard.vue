b646555c1af412404b20d7faa29c42c317c458ca
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-25 09:22:33 -0600

    styled up album card

--- client/src/components/AlbumCard.vue
+++ client/src/components/AlbumCard.vue
@@ -8,8 +8,34 @@ defineProps({
 
 
 <template>
-  {{ album.title }}
+  <div class="rounded shadow p-2 album-card mb-3 d-flex align-items-end"
+    :style="{ backgroundImage: `url(${album.coverImg})` }">
+    <div class="d-flex justify-content-between align-items-center bg-dark-glass rounded p-2 flex-grow-1">
+      <!-- TODO show off random color -->
+      <div>
+        <p class="fw-bold mb-1">{{ album.title }}</p>
+        <div>
+          <img :src="album.creator.picture" :alt="'A picture of ' + album.creator.name" class="round-picture me-2">
+          <span>{{ album.creator.name }}</span>
+        </div>
+      </div>
+      <div>
+        <span class="mdi mdi-account-multiple me-2"></span>
+        <span>0</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.album-card {
+  min-height: 30dvh;
+  background-size: cover;
+  background-position: center;
+}
+
+img {
+  height: 2rem;
+}
+</style>
