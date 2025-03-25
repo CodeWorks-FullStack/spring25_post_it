9e4d0b0edffddf8297f23b492f78551bec32af96
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-25 12:16:47 -0600

    getting watchers by album id

--- server/src/services/WatchersService.js
+++ server/src/services/WatchersService.js
@@ -1,11 +1,18 @@
 import { dbContext } from "../db/DbContext.js"
 
 class WatchersService {
+
   async createWatcher(watcherData) {
     const watcher = await dbContext.Watchers.create(watcherData)
     await watcher.populate('profile', 'name picture')
     await watcher.populate('album')
     return watcher
+  }
+
+  async getWatchersByAlbumId(albumId) {
+    //                                             { albumId: '67e2f12f43efcaffa6899d82' }
+    const watchers = await dbContext.Watchers.find({ albumId: albumId }).populate('profile', 'name picture')
+    return watchers
   }
 }
 
