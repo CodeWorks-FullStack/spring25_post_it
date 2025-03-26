4413a924e6df9887af76fe4dc09d05345f18c47d
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-26 09:52:57 -0600

    storing watcher profiles in appstate

--- client/src/services/WatchersService.js
+++ client/src/services/WatchersService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Watcher } from "@/models/Watcher.js"
+import { AppState } from "@/AppState.js"
 
 class WatchersService {
   async getWatchersByAlbumId(albumId) {
     const response = await api.get(`/api/albums/${albumId}/watchers`)
     logger.log('GOT WATCHERS', response.data)
+    const watcherProfiles = response.data.map(pojo => new Watcher(pojo))
+    AppState.watcherProfiles = watcherProfiles
   }
 }
 
